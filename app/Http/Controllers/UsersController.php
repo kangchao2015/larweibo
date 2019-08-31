8966f136df47a4adc1bcd1cc8805887ae8cb3add
kangchao2015 <kangchaolove2009@163.com>
2019-08-31 11:05:55 +0800

    权限系统

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,19 @@ use Illuminate\Support\Facades\Log;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+          'except' => [
+            'create', 'store','index'
+          ]
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function index()
     {
         echo 123;
@@ -25,6 +38,8 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
     }
 
@@ -52,6 +67,8 @@ class UsersController extends Controller
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
+
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
